1a9a0e18104ed51c9f172e88926991efa6893617
Tatenda Midzi <tatendamidzi49@gmail.com>
2025-04-15 12:16:21 +0000

    Made compound interest calculator

---
+++ Projects/compoundInterest.java
@@ -0,0 +1,32 @@
+package Projects;
+import java.util.Scanner;
+
+public class compoundInterest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double amt;
+        double rate;
+        double cmp;
+        int years;
+        double total;
+
+        System.out.print("Enter the principle amount: ");
+        amt = scanner.nextDouble();
+
+        System.out.println("Enter the interest rate (in %): ");
+        rate = scanner.nextDouble();
+
+        System.out.println("Enter the # of times compounded per year: ");
+        cmp = scanner.nextDouble();
+
+        System.out.println("Enter the # of years: ");
+        years = scanner.nextInt();
+
+        total = amt * (100 + rate) / 100;
+
+        System.out.println(total);
+        
+        scanner.close();
+    }
+}
