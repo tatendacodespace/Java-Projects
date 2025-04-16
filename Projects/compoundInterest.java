37f48d0e992d76a30b9d83dfdc83cf8c9dcdd271
Tatenda Midzi <tatendamidzi49@gmail.com>
2025-04-16 16:54:15 +0000

    Added comment to decribe the code

--- Projects/compoundInterest.java
+++ Projects/compoundInterest.java
@@ -1,6 +1,7 @@
 package Projects;
 import java.util.Scanner;
 
+// Compound intterest calculator
 public class compoundInterest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
